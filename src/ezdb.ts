1aeb1b44c22d30d5bac6ad9f4aa77829124adacf
Felipe Dias <faadias@gmail.com>
2018-01-18 12:04:30 -0200

    Added a console.log for when indexedDB is not supported

--- src/ezdb.ts
+++ src/ezdb.ts
@@ -2,6 +2,11 @@ import DBManager from "./dbmanager";
 import { EZDBUpdateType, EZDBQueryReturn } from "./enums";
 
 (function(){
+	if (!this.indexedDB) {
+		console.log("IndexedDB not supported!");
+		return;
+	}
+
 	this.ezdb = DBManager.Instance;
 	this.EZDBUpdateType = EZDBUpdateType;
 	this.EZDBQueryReturn = EZDBQueryReturn;
